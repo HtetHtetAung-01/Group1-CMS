ec7ce96977cb3cbb8f0e5593204a5a32127b2d27
tyikyi <ddale910@gmail.com>
2021-11-16 09:37:30 +0630

    [feature/auth] add Authentication

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//registration
+Route::get('dashboard', [AuthController::class, 'userDashboard']); 
+Route::get('login', [AuthController::class, 'Index'])->name('login');
+Route::post('custom-login', [AuthController::class, 'userCustomLogin'])->name('login.custom'); 
+Route::get('registration', [AuthController::class, 'userRegistration'])->name('register-user');
+Route::post('custom-registration', [AuthController::class, 'userCustomRegistration'])->name('register.custom'); 
+Route::get('signout', [AuthController::class, 'signOut'])->name('signout');
+
+//forgetpassword
+Route::get('forget-password', [ForgetPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+Route::post('forget-password', [ForgetPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
+Route::get('reset-password/{token}', [ForgetPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('reset-password', [ForgetPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+
+//user
+Route::get('/user-list', [AuthController::class, 'showUserList']); 
+Route::delete('/user/{id}', [AuthController::class, 'deleteUser']);
+Route::get('/userdetail/{id}', [AuthController::class, 'userDetail']); 
+Route::get('/useredit/{id}', [AuthController::class, 'editUser']); 
+Route::post('/update/{id}',[AuthController::class, 'updateUser']);
