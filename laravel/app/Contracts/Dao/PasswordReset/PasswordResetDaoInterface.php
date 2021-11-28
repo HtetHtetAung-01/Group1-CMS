7f8069f6055c5a2188e4fcceebffc6e281674ec4
devyza <freeman404.it@gmail.com>
2021-11-29 01:41:32 +0630

    [codeFormat/tk] add comment to PasswordResetDao

--- laravel/app/Contracts/Dao/PasswordReset/PasswordResetDaoInterface.php
+++ laravel/app/Contracts/Dao/PasswordReset/PasswordResetDaoInterface.php
@@ -4,7 +4,23 @@ namespace App\Contracts\Dao\PasswordReset;
 
 interface PasswordResetDaoInterface
 {
+    /**
+     * To insert password reset token record
+     * @param Request $record
+     */
     public function insertPasswordReset($record);
+
+    /**
+     * To get password reset record by email and token
+     * @param string $email user's email
+     * @param string $token password reset token
+     * @return Object
+     */
     public function getPasswordResetByEmailAndToken($email, $token);
+
+    /**
+     * To delete password reset token record by email
+     * @param string $email user's email
+     */
     public function deletePasswordResetbyEmail($email);
 }
--- laravel/app/Dao/PasswordReset/PasswordResetDao.php
+++ laravel/app/Dao/PasswordReset/PasswordResetDao.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\DB;
 
 class PasswordResetDao implements PasswordResetDaoInterface
 {
+    /**
+     * To insert password reset token record
+     * @param Request $record
+     */
     public function insertPasswordReset($request)
     {
         DB::table('password_resets')
@@ -18,6 +22,12 @@ class PasswordResetDao implements PasswordResetDaoInterface
             ]);
     }
     
+    /**
+     * To get password reset record by email and token
+     * @param string $email user's email
+     * @param string $token password reset token
+     * @return Object
+     */
     public function getPasswordResetByEmailAndToken($email, $token)
     {
         return DB::table('password_resets')
@@ -25,6 +35,10 @@ class PasswordResetDao implements PasswordResetDaoInterface
             ->first();
     }
 
+    /**
+     * To delete password reset token record by email
+     * @param string $email user's email
+     */
     public function deletePasswordResetbyEmail($email)
     {
         DB::table('password_resets')
