92ac083537424236f2666cec3ca727fc1879bb58
KhwarNyoThin <khwarnyo1999@gmail.com>
2021-12-02 11:10:28 +0630

    [feature/responsive] Fix Auth Responsive

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -11,40 +11,38 @@
         <!-- add narvar -->
         <div class="box-root">
             <div class="formbg">
-                <div class="formbg-inner">
-                    <span class="login-title">Login</span>
-                    @if (Session::has('message'))
-                        <div class="alert alert-success" role="alert">
-                            {{ Session::get('message') }}
+                <span class="login-title">Login</span>
+                @if (Session::has('message'))
+                    <div class="alert alert-success" role="alert">
+                        {{ Session::get('message') }}
+                    </div>
+                @endif
+                <form class="login-form" form action="{{ route('login-custom') }}" method="POST">
+                    @csrf
+                    <div class="text">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email">
+                        @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+                    <div class="text">
+                        <label for="password">Password</label>
+                        <input type="password" name="password">
+                        @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                    <div class="text">
+                        <div class="reset-pass">
+                            <a href="{{ route('forget.password.get') }}">Forgot your password?</a>
                         </div>
-                    @endif
-                    <form class="login-form" form action="{{ route('login-custom') }}" method="POST">
-                        @csrf
-                        <div class="text">
-                            <label for="email">E-mail</label>
-                            <input type="email" name="email">
-                            @if ($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                            @endif
-                        </div>
-                        <div class="text">
-                            <label for="password">Password</label>
-                            <input type="password" name="password">
-                            @if ($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                            @endif
-                        </div>
-                        <div class="text">
-                            <div class="reset-pass">
-                                <a href="{{ route('forget.password.get') }}">Forgot your password?</a>
-                            </div>
-                        </div>
-                        <div class="text">
-                            <button class="login-btn">Login</button>
-                        </div>
-                        <p>Don't have an account?<a href="{{ route('register-user') }}"> Register Now</a></p>
-                    </form>
-                </div>
+                    </div>
+                    <div class="text">
+                        <button class="login-btn">Login</button>
+                    </div>
+                    <p>Don't have an account?<a href="{{ route('register-user') }}"> Register Now</a></p>
+                </form>
             </div>
         </div>
     </div>
